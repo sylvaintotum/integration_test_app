ebabac2c10e98d8fd8c5d40a816c592f352af6db
sylvaintotum <sylvain@totumpass.com>
2012-07-12 15:15:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IntegrationTestApp::Application.config.secret_token = '4240334d1b1f55dc48673cbcd0f0c049c54e60ee8b8808aa071200e2456767fedeeedcbd494751a2d6e334fbe050287feab16cbbd1731b5a4b2bc8c0cf752b63'
